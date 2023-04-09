50527270075ab36f98d70b425b59ea3ab976f6fd
JoeLee <lizhou1021@qq.com>
2023-04-09 22:47:33 +0800

    feat:散开动画

--- packages/model-particle/src/app/app.tsx
+++ packages/model-particle/src/app/app.tsx
@@ -50,11 +50,17 @@ export function App() {
 
       let mtl;
 
+      let k = 0;
+
+      let isReady = false;
+
+      let pcs: PointsCloudSystem;
+
       SceneLoader.ImportMesh("", "/models/", "seagulf.glb", scene, (meshes) => {
         // console.log('assets: ', assets);
         // assets.addAllToScene();
 
-        const pcs = new PointsCloudSystem("pcs", 2, scene);
+        pcs = new PointsCloudSystem("pcs", 2, scene);
 
         model = meshes[0] as Mesh;
         // meshes[0].setEnabled(false);
@@ -63,54 +69,51 @@ export function App() {
 
         model.position.x = -100;
 
-        // pcs.initParticles = function () {
-        //   const bb = pcs.mesh!.getBoundingInfo().boundingBox;
-        //   let bbmin = bb.minimumWorld.y;
-
-        //   for (let p = 0; p < pcs.nbParticles; p++) {
-        //     pcs.particles[p].velocity = Vector3.Zero();
-        //     let ay = (pcs.particles[p].position.y - bbmin) / 500;
-        //     let ax = Scalar.RandomRange(-ay, ay);
-        //     let az = Scalar.RandomRange(-ay, ay);
-        //     pcs.particles[p].acceleration = new Vector3(0.25 * ax, 0.5 * Math.abs(ay) * (1 + Math.random()), 0.25 * az);
-        //     let c = Scalar.RandomRange(0, 0.5);
-        //     pcs.particles[p].color = new Color4(c, c, c, 1);
-        //   }
-        // };
-
-        // pcs.updateParticle = function (particle) {
-        //   particle.velocity.addInPlace(particle.acceleration);
-        //   particle.position.addInPlace(particle.velocity);
-        // };
-
-        pcs.addSurfacePoints(meshes[1] as Mesh, 100000, PointColor.Color, 0);
+        pcs.addSurfacePoints(meshes[1] as Mesh, 50000, PointColor.Color, 0);
 
         pcs.buildMeshAsync().then(() => {
           // pcs.mesh?.setEnabled(false);
+          pcs.setParticles();
         });
 
-        // model.setEnabled(false);
-
-        // assets.meshes.forEach((mesh) => {
-        //   mesh.visibility = 0.1;
-        // });
         meshes[1].renderingGroupId = 1;
         meshes[1].material!.clipPlane = plane;
         mtl = meshes[1].material;
 
+
+        pcs.updateParticle = function (particle) {
+          if (isReady) {
+            particle.velocity.addInPlace(new Vector3(Scalar.RandomRange(0, 2), Scalar.RandomRange(0, 2), Scalar.RandomRange(0, 2)));
+            particle.position.addInPlace(new Vector3(Scalar.RandomRange(0, 100), 2, Scalar.RandomRange(0, 100)));
+
+          }
+        };
+
       });
+
+
 
       // Run the render loop
       engine.runRenderLoop(() => {
         scene.render();
       });
 
-      scene.onAfterRenderObservable.add(() => {
+      let i = 0;
+      scene.onBeforeRenderObservable.add(() => {
         if (plane.d >= 800) {
           if (model) {
             model.setEnabled(false);
           }
-          return;
+          isReady = true;
+          if (isReady) {
+            pcs.setParticles(i, i + 20000);
+          }
+          i += 100;
+
+          if (i >= 30000) {
+            pcs.dispose();
+
+          }
         }
         else {
           plane.d += 5;
